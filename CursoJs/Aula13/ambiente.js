90107ae7f38e24efaa49504e09fa02cd39490413
Maxwel Robson <maxps2@live.com>
2024-09-18 12:12:22 -0300

    .

---
+++ CursoJs/Aula13/ambiente.js
@@ -0,0 +1,14 @@
+var c = 1
+do { //faça isso 
+    console.log(`Passo ${c}`) // escreva "Passo (valor de c)"
+    c++ //soma mais um ao valor de c
+} while (c <= 5)  //enquanto o valor de c for menor ou igual a 5
+
+
+/*
+var c = 1
+while (c <= 10) { //enquanto o valor de c for menor ou igual a 10
+    console.log(`Passo ${c}`) // escreva "Passo (valor de c)"
+    c++ //soma mais um ao valor de c
+}
+*/
