d9ef63ebd91f883419e1dd2e0a0b336aadcb9e19
Maxwel Robson <127208401+maxwelrobson@users.noreply.github.com>
2024-09-18 14:48:38 -0300

    .

--- CursoJs/Aula13/ambiente.js
+++ CursoJs/Aula13/ambiente.js
@@ -5,10 +5,9 @@ do { //faça isso
 } while (c <= 5)  //enquanto o valor de c for menor ou igual a 5
 
 
-/*
+
 var c = 1
 while (c <= 10) { //enquanto o valor de c for menor ou igual a 10
     console.log(`Passo ${c}`) // escreva "Passo (valor de c)"
     c++ //soma mais um ao valor de c
 }
-*/
