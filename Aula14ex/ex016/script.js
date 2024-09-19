5927c68a6d025418a80edc975f0880f5538113fd
Maxwel Robson <127208401+maxwelrobson@users.noreply.github.com>
2024-09-19 16:34:45 -0300

    .

--- Aula14ex/ex016/script.js
+++ Aula14ex/ex016/script.js
@@ -1,24 +1,24 @@
 function contar() {
-    let ini = document.getElementById('txti')
-    let fim = document.getElementById('txtf')
-    let passo = document.getElementById('txtp')
-    let res = document.getElementById('res')
+    let ini = document.getElementById('txti') //Pega o valor que foi escrito no input Incio
+    let fim = document.getElementById('txtf') //Pega o vaolr que foi escrito no input Final
+    let passo = document.getElementById('txtp') //Pega o valor que fo escrito no input Passo
+    let res = document.getElementById('res') //Variável criada para guardar o valor da resposta
 
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
-        res.innerHTML = 'Impossível Contar!'
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) { //Se o valor de Inicio for iual a 0 OU valor de Fim for igual a zero OU valor de Passo for igual a 0
+        window.alert('[ERRO] Faltam dados!')  //Faça um alerta com o que está escrito dentro dos parenteses
+        res.innerHTML = 'Impossível Contar!' //Escreva "Impossível Contar!" no campo(div) da resposta
     } else {
-        res.innerHTML = 'Contando: <br>'
-        let i = Number(ini.value)
+        res.innerHTML = 'Contando: <br>' //Escreve "Contando antes da resposta"
+        let i = Number(ini.value)   //Cria novas variáveis convertendo para número os valores que foram "coletados"
         let f = Number(fim.value)
         let p = Number(passo.value)
-        if (p <= 0) {
-            window.alert('PAsso inválido! Considerando PASSO 1')
-            p = 1
+        if (p <= 0) { //Se o valor de Passo for menor igual a zero
+            window.alert('PAsso inválido! Considerando PASSO 1') //Crie um alerta dizer o que está estre parenteses
+            p = 1 //Faz o valor de p ser igual a 1
         }
 
-        if (i < f) {
-            for (let c = i; c <= f; c += p) {
+        if (i < f) { //Se o valor de Inicio for menor que o do Final
+            for (let c = i; c <= f; c += p) { //Para 
                 res.innerHTML += `${c} \u{1F449}`
             }
             
